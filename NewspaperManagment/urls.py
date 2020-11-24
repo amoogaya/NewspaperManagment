d289849c04162093dd68613d1502e50b1eeb6f18
aya gomaa <aya.goomaa@gmail.com>
2020-11-24 09:20:56 +0200

    step1

--- NewspaperManagment/urls.py
+++ NewspaperManagment/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path ,include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djrichtextfield/', include('djrichtextfield.urls')),
     path('newspaper/', include('newspaper.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
