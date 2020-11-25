586808f3e1bf553421bf16c3e786d29ce4eec164
aya gomaa <aya.goomaa@gmail.com>
2020-11-25 15:50:58 +0200

    blog using wegtail

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.http import JsonResponse
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, PageChooserPanel, InlinePanel
 from wagtail.core.fields import RichTextField
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.documents.edit_handlers import DocumentChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
@@ -11,10 +12,16 @@ class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro', classname='full')
+        FieldPanel('intro', classname='full'),
     ]
 
-    subpage_types = ['blog.BlogPage']
+    subpage_types = ['blog.BlogPage',]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(self, request, *args, **kwargs)
+
+        context['blog_entries'] = BlogPage.objects.child_of(self).live()
+        return context
 
 
 class BlogPage(Page):
@@ -31,6 +38,8 @@ class BlogPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('body', classname="full"),
+        InlinePanel('related_links'),
+
     ]
 
     search_fields = Page.search_fields + [
@@ -43,5 +52,24 @@ class BlogPage(Page):
         ImageChooserPanel('feed_image'),
     ]
 
-    parent_page_types = ['blog.BlogIndexPage',]
+    parent_page_types = ['blog.BlogIndexPage', ]
     subpage_types = []
+
+    def serve(self, request):
+        return JsonResponse({
+            'title': self.title,
+            'body': self.body,
+            'date': self.date,
+            'feed_image': self.feed_image.get_rendition('width-30').url,
+        })
+
+
+class BlogPageRelatedLink(Orderable):
+    blog_page = models.ForeignKey(BlogPage, on_delete=models.CASCADE, related_name='related_links')
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('url'),
+    ]
